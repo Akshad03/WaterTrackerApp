bd7fda3fd0d44d11c47e6ffe2c23ec283d8c7a6a
Akshad03 <akshadwalunj1204@gmail.com>
2026-05-04 19:47:38 +0530

    feat: final UI cleanup and fixed navigation routing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,19 @@
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
 
-// 1. Remove the unstable_settings anchor to (tabs)
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}> 
-        {/* 'headerShown: false' here hides it for EVERY screen */}
-        <Stack.Screen name="index" options={{ title: 'Hydration Tracker' }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        {/* Set (tabs) as the main route group */}
+        <Stack.Screen
+          name="(tabs)"
+          options={{ headerShown: false }}
+        />
       </Stack>
-      <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
